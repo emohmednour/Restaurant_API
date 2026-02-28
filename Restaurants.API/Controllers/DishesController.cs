5f0cba7e85d163a0bc6b1c1b8bc0971057f0d25f
Mohmed Nour Eldeen <emohmednour@gmail.com>
2026-02-28 12:05:08 +0200

    add massage in notfoundException

---
+++ Restaurants.API/Controllers/DishesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurants.API.Controllers
+{
+    public class DishesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Restaurants.Domain/Exceptions/NotFoundException.cs
+++ Restaurants.Domain/Exceptions/NotFoundException.cs
@@ -2,6 +2,7 @@
 
 namespace Restaurants.Domain.Exceptions;
 
-public class NotFoundException(string? message) : Exception(message)
+public class NotFoundException(string resourceType, string resourceIdentifier) 
+    : Exception($"{resourceType} with id : {resourceIdentifier} dosen't exist")
 {
 }
