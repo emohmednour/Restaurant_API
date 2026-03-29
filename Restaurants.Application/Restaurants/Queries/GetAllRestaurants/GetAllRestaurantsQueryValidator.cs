58c112b28a8c7a06fe9c8964dfb5a3335a2afe3c
Mohmed Nour Eldeen <emohmednour@gmail.com>
2026-03-29 18:31:09 +0200

    Pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿
+
+using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+
+    private int[] allowedPagedSize = [5,10,15,30];
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+            //.InclusiveBetween(1, 50)
+        
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowedPagedSize.Contains(value)).
+            WithMessage($"Page size must be in [{string.Join(",",allowedPagedSize)}]");
+     }
+
+}
+
